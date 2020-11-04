c362ef69e7f11ce76bc6fe9411d52d59fe694d37
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2020-11-04 14:13:34 -0800

    CallToolV1.py
    An archive of the first version of the phone call tool.

---
+++ PhoneHome/Phone/OldVersions/CallToolV1.py
@@ -0,0 +1,21 @@
+# Start of script
+print ("Dial a number:")
+phoneNumberInput = int(input(":\\")
+print ("Are you ready to phone home? " + phoneNumberInput())
+confirmCallYN = str(input("(Y/N):"))
+answer = confirmCallYN.upper()
+if (answer == "Y"):
+  print ("Dialing")
+  print ("Error: this feature is not yet available")
+else:
+  print ("Phone call request canceled")
+print ("Do you want to listen to the call instead?")
+noMore = input("Press [ENTER] key to quit")
+print ("The program should be closed. If the program is still open, try closing the window. If this doesn't work, try killing the process with a process manager or task manager")
+# End of script
+"""
+File info
+File version: 1 (Tuesday, November 4th 2020 at 2:13 pm)
+File type: Python script file (*.py)
+Line count (including blank lines and compiler line): 22
+"""
